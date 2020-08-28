fb98b5c67b8e27a545e4e18e3ded0023ce244dc9
Andrii Baran <andriy.baran.v@gmail.com>
2020-08-28 09:59:02 +0300

    Updated specs

--- spec/simple_facade_delegation_spec.rb
+++ spec/simple_facade_delegation_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe SimpleFacade do
   let(:facade) { SimpleFacade::Base.new }
   let(:a_class) { Class.new{ def a; 'a'; end} }
-  let(:b_class) { Class.new{ def b; 'b'; end} }
-  let(:c_class) { Class.new{ def c; 'c'; end} }
+  let(:b_class) { Class.new{ def mb; 'b'; end} }
+  let(:c_class) { Class.new{ def mc; 'c'; end} }
 
   it 'delegates methods to members' do
     obj1 = a_class.new
@@ -11,14 +11,17 @@ RSpec.describe SimpleFacade do
     facade.push(:a, obj1)
     facade.push(:b, obj2)
     facade.push(:c, obj3)
-    expect(facade.a).to eq 'a'
-    expect(facade.b).to eq 'b'
-    expect(facade.c).to eq 'c'
+    expect(facade.a).to eq obj1
+    expect(facade.b).to eq obj2
+    expect(facade.c).to eq obj3
+    expect(facade.a.a).to eq 'a'
+    expect(facade.mb).to eq 'b'
+    expect(facade.mc).to eq 'c'
     expect{ facade.d }.to raise_error(NoMethodError)
     facade.dequeue
     expect{ facade.a }.to raise_error(NoMethodError)
     facade.pop
     expect{ facade.c }.to raise_error(NoMethodError)
-    expect(facade.b).to eq 'b'
+    expect(facade.mb).to eq 'b'
   end
 end
